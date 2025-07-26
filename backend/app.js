3bcbdb662bc885604945a0d014df203cd03427fc
arpan0137 <arpanmalaviya2003@gmail.com>
2025-07-26 22:21:04 +0530

    feat(auth): make registration test pass with mock response.

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,17 @@
 import express from "express";
 const app = express();
 
+// Middleware to parse JSON bodies
+app.use(express.json());
+
 // A simple route to test the server
 app.get('/', (req, res) => {
     res.send('Backend server is running!');
+});
+
+// route for user registration
+app.post('/api/auth/register',(req,res) => {
+    res.status(201).json({message: "User registerd successfully"});
 });
 
 export default app
